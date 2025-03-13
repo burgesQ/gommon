009fe0af971e15d7036050517dcf9f24cb41261e
Quentin Burgess <qutn.burgess@gmail.com>
2025-03-13 12:02:49 +0100

    feat(mtls): support for merged file hash in config

--- mtls/config.go
+++ mtls/config.go
@@ -4,16 +4,26 @@ import "log/slog"
 
 // Config contain the tls config passed by the config file.
 type Config struct {
-	Cert     string `json:"cert"     mapstructure:"cert"`
-	Key      string `json:"key"      mapstructure:"key"`
-	Ca       string `json:"ca"       mapstructure:"ca"`
-	Level    Level  `json:"level"    mapstructure:"level"`
-	Insecure bool   `json:"insecure"    mapstructure:"insecure"`
+	// Hash is a unique hash of the cert + key + ca content.
+	Hash string `json:"hash" mapstructure:"hash"`
+	// Cert is the path to the TLS certificate.
+	Cert string `json:"cert"     mapstructure:"cert"`
+	// Key is the path to the TLS key.
+	Key string `json:"key"      mapstructure:"key"`
+	// CA is the path to the TLS CA certificate.
+	Ca string `json:"ca"       mapstructure:"ca"`
+	// Level TLS authentication level.
+	Level Level `json:"level"    mapstructure:"level"`
+	// Insecure is true if insecure TLS is allowed (client).
+	Insecure bool `json:"insecure"    mapstructure:"insecure"`
 }
 
 func (cfg Config) SameAs(in Config) bool {
-	return cfg.Cert == in.Cert && cfg.Key == in.Key &&
-		cfg.Ca == in.Ca && cfg.Level == in.Level
+	return cfg.Hash == in.Hash &&
+		cfg.Cert == in.Cert &&
+		cfg.Key == in.Key &&
+		cfg.Ca == in.Ca &&
+		cfg.Level == in.Level
 }
 
 // // GetCert implemte Config.
@@ -27,8 +37,10 @@ func (cfg Config) SameAs(in Config) bool {
 
 // func (cfg Config) GetLevel() Level { return cfg.Level }
 
-// Empty implemte Config.
-func (cfg Config) Empty() bool { return cfg.Cert == "" && cfg.Key == "" && !cfg.Insecure }
+// Empty implement Config.
+func (cfg Config) Empty() bool {
+	return cfg.Hash == "" && cfg.Cert == "" && cfg.Key == "" && !cfg.Insecure
+}
 
 func (cfg Config) AsAttrs() []any {
 	if cfg.Empty() {
@@ -36,7 +48,12 @@ func (cfg Config) AsAttrs() []any {
 	}
 
 	return []any{
-		slog.String("cert", cfg.Cert), slog.String("key", cfg.Key),
-		slog.String("CA", cfg.Ca), slog.String("level", cfg.Level.String()),
+		slog.String("cert", cfg.Cert),
+		slog.String("key", cfg.Key),
+		slog.String("CA", cfg.Ca),
+		slog.String("hash", cfg.Hash),
+
+		slog.String("level", cfg.Level.String()),
+		slog.Bool("insecure (client)", cfg.Insecure),
 	}
 }
